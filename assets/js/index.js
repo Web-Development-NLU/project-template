72ac07906574d80fc1e6efb2c63afb278026c1a7
quangtho908 <quangtho23062002@gmail.com>
2022-10-28 17:41:27 +0700

    processing section filter, add section menu, processing section silder

---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+import "./menu-responsive.js"
+import "./menu-scroll.js"
+import "./slider.js"
---
+++ assets/js/slider.js
@@ -0,0 +1,7 @@
+const slider = tns({
+  container: ".head_bottom-slider",
+  items: 1,
+  mouseDrag: true,
+  controls: false,
+  navPosition: "bottom"
+})
