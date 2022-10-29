9f04e8bd24d86ce9bbf73e53f809d013f5ab1c03
quangtho908 <quangtho23062002@gmail.com>
2022-10-29 19:04:05 +0700

    update compoents, handle gallery, add head-bottom-page, add slide-attachment

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
 import "./menu-responsive.js"
 import "./menu-scroll.js"
 import "./slider.js"
+import "./product-gallery.js"
---
+++ assets/js/product-gallery.js
@@ -0,0 +1,16 @@
+const gallery = document.querySelector('.product-gallery')
+
+if(gallery) {
+  const slider = tns({
+    container: ".product-gallery",
+    item: 1,
+    controls: false,
+    mouseDrag: true,
+    navContainer: ".gallery-action",
+    respnsive: {
+      991: {
+        nav: false
+      }
+    }
+  })
+}
